379d964eb047d950b0318e2b6f90355458e0b6a2
Nicolas Lara <nicolaslara@gmail.com>
2023-11-03 14:46:02 +0100

    more boilerplate

--- pools/usecase/oracle_usecase.go
+++ pools/usecase/oracle_usecase.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"context"
 	"fmt"
+	concentrated "github.com/osmosis-labs/osmosis/v19/x/concentrated-liquidity/model"
 	"github.com/osmosis-labs/sqs/chain"
 	"time"
 
@@ -33,12 +34,45 @@ func (a *oracleUseCase) UpdatePrices(ctx context.Context) error {
 		panic(err)
 	}
 
-	cfmmPools, err := a.client.GetAllPools(ctx, height)
+	allPools, err := a.client.GetAllPools(ctx, height)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(cfmmPools)
+	var osmoPool domain.PoolI
+	found := false
+	osmoPoolId := uint64(1)
+	for _, pool := range allPools {
+		if pool.GetId() == osmoPoolId {
+			osmoPool = pool
+			found = true
+		}
+	}
+
+	if !found {
+		return fmt.Errorf("osmo pool not found")
+	}
+
+	// TODO: Calculate the price
+	price, conf, err := calculateCLPrice(osmoPool)
+	if err != nil {
+		return err
+	}
+
+	// TODO: Update the price in PYTH
+	// i.e.: call update_price on the websocket api of the pyth-agent: https://docs.pyth.network/documentation/publish-data/pyth-client-websocket-api#update_price
+	// Account, Price, Conf (confidence), Status
+	fmt.Println("price: ", price, "conf: ", conf)
 
 	return nil
+}
+
+func calculateCLPrice(pool domain.PoolI) (int64, uint64, error) {
+	cl, ok := pool.(concentrated.Pool)
+	if !ok {
+		panic("invalid pool type")
+	}
+
+	// TODO: Much better price calculations here
+	return cl.CurrentSqrtPrice.RoundInt64(), 0, nil
 }
