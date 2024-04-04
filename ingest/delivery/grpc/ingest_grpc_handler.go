28b7480605c34ddffcf5fa12007ffb048ab7c7f0
Roman <roman@osmosis.team>
2024-04-03 23:30:24 -0600

    update ingest usecase and handler (#156)

--- ingest/delivery/grpc/ingest_grpc_handler.go
+++ ingest/delivery/grpc/ingest_grpc_handler.go
@@ -2,7 +2,6 @@ package grpc
 
 import (
 	"context"
-	"io"
 	"time"
 
 	"github.com/osmosis-labs/sqs/domain"
@@ -37,46 +36,7 @@ func NewIngestGRPCHandler(us mvc.IngestUsecase, grpcIngesterConfig domain.GRPCIn
 }
 
 // ProcessChainPools implements types.IngesterServer.
-func (i *IngestGRPCHandler) ProcessChainPools(stream prototypes.SQSIngester_ProcessChainPoolsServer) (err error) {
-	var poolDataChunk prototypes.ChainPoolsDataChunk
-	err = stream.RecvMsg(&poolDataChunk)
-	for err == nil {
-		err = i.ingestUseCase.ProcessPoolChunk(stream.Context(), poolDataChunk.Pools)
-		if err != nil {
-			return err
-		}
+func (i *IngestGRPCHandler) ProcessBlock(ctx context.Context, req *prototypes.ProcessBlockRequest) (*prototypes.ProcessBlockReply, error) {
 
-		err = stream.RecvMsg(&poolDataChunk)
-	}
-
-	if err != io.EOF {
-		return err
-	}
-
-	return stream.SendAndClose(&prototypes.ProcessChainPoolsReply{})
-}
-
-// StartBlockProcess( implements types.IngesterServer.
-func (i *IngestGRPCHandler) StartBlockProcess(ctx context.Context, req *prototypes.StartBlockProcessRequest) (resp *prototypes.StartBlockProcessReply, err error) {
-	takerFeeMap := sqsdomain.TakerFeeMap{}
-
-	if err := takerFeeMap.UnmarshalJSON(req.TakerFeesMap); err != nil {
-		return nil, err
-	}
-
-	// Start block processing with the taker fees.
-	if err := i.ingestUseCase.StartBlockProcess(ctx, req.BlockHeight, takerFeeMap); err != nil {
-		return nil, err
-	}
-
-	return &prototypes.StartBlockProcessReply{}, nil
-}
-
-// EndBlockProcessing implements types.IngesterServer.
-func (i *IngestGRPCHandler) EndBlockProcess(ctx context.Context, req *prototypes.EndBlockProcessRequest) (resp *prototypes.EndBlockProcessReply, err error) {
-	if err := i.ingestUseCase.EndBlockProcess(ctx, req.BlockHeight); err != nil {
-		return nil, err
-	}
-
-	return &prototypes.EndBlockProcessReply{}, nil
+	return &prototypes.ProcessBlockReply{}, nil
 }
