a4e70faac3622d52fb10f8bc89855afdf808528b
Roman <roman@osmosis.team>
2024-05-11 23:38:15 +0000

    POC: Flight recording of slow requests (above 500ms)

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,11 +1,20 @@
 package middleware
 
 import (
+	"bytes"
 	"context"
+	"fmt"
+	"os"
+	"sync"
+
 	"time"
+
+	"go.uber.org/zap"
+	gotrace "golang.org/x/exp/trace"
 
 	"github.com/labstack/echo/v4"
 	"github.com/osmosis-labs/sqs/domain"
+	"github.com/osmosis-labs/sqs/log"
 	"github.com/prometheus/client_golang/prometheus"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
@@ -16,7 +25,15 @@ import (
 // GoMiddleware represent the data-struct for middleware
 type GoMiddleware struct {
 	corsConfig domain.CORSConfig
+	logger     log.Logger
 }
+
+const (
+	// Name of the flight recorder trace
+	flightRecorderTraceName = "flight-trace.out"
+	// The latency threshold in milliseconds to record traces
+	flightRecorderThresholdMs = 500
+)
 
 var (
 	// total number of requests counter
@@ -37,6 +54,9 @@ var (
 		},
 		[]string{"method", "endpoint"},
 	)
+
+	// flight recorder
+	recordFlightOnce sync.Once
 )
 
 func init() {
@@ -55,14 +75,23 @@ func (m *GoMiddleware) CORS(next echo.HandlerFunc) echo.HandlerFunc {
 }
 
 // InitMiddleware initialize the middleware
-func InitMiddleware(corsConfig *domain.CORSConfig) *GoMiddleware {
+func InitMiddleware(corsConfig *domain.CORSConfig, logger log.Logger) *GoMiddleware {
 	return &GoMiddleware{
 		corsConfig: *corsConfig,
+		logger:     logger,
 	}
 }
 
 // InstrumentMiddleware will handle the instrumentation middleware
 func (m *GoMiddleware) InstrumentMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+
+	// Set up the flight recorder.
+	fr := gotrace.NewFlightRecorder()
+	err := fr.Start()
+	if err != nil {
+		m.logger.Error("failed to start flight recorder", zap.Error(err))
+	}
+
 	return func(c echo.Context) error {
 		start := time.Now()
 
@@ -83,10 +112,41 @@ func (m *GoMiddleware) InstrumentMiddleware(next echo.HandlerFunc) echo.HandlerF
 
 		err = next(c)
 
-		duration := time.Since(start).Seconds()
+		duration := time.Since(start)
 
 		// Observe the duration with the histogram
-		requestLatency.WithLabelValues(requestMethod, requestPath).Observe(duration)
+		requestLatency.WithLabelValues(requestMethod, requestPath).Observe(duration.Seconds())
+
+		// Record outliers to the flight recorder for futher analysis
+		if duration > flightRecorderThresholdMs*time.Millisecond {
+
+			recordFlightOnce.Do(func() {
+				// Note: we skip error handling since we don't want to interrupt the request handling
+				// with tracing errors.
+
+				// Grab the snapshot.
+				var b bytes.Buffer
+				_, err = fr.WriteTo(&b)
+				if err != nil {
+					m.logger.Error("failed to write trace to buffer", zap.Error(err))
+					return
+				}
+
+				// Write it to a file.
+				err = os.WriteFile(flightRecorderTraceName, b.Bytes(), 0o755)
+				if err != nil {
+					m.logger.Error("failed to write trace to file", zap.Error(err))
+					return
+				}
+
+				err = fr.Stop()
+				if err != nil {
+					fmt.Println("failed to stop flight recorder: ", err)
+					m.logger.Error("failed to stop fligt recorder", zap.Error(err))
+					return
+				}
+			})
+		}
 
 		return err
 	}
