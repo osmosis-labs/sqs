733d0c001257607004ef725d9404bd2252dd1bd1
Calvin <calvinchso@gmail.com>
2024-05-22 19:50:17 +0000

    Fixed linter complaint

--- domain/mocks/pricing_coingecko_mock.go
+++ domain/mocks/pricing_coingecko_mock.go
@@ -12,7 +12,7 @@ var (
 	oneBigDec  = osmomath.NewBigDec(1)
 )
 
-// MockCoingeckoPriceGetter is a mock implementation of CoingeckoPriceGetterFn
+// DefaultMockCoingeckoPriceGetter is a mock implementation of CoingeckoPriceGetterFn
 var DefaultMockCoingeckoPriceGetter coingeckopricing.CoingeckoPriceGetterFn = func(ctx context.Context, baseDenom string, coingeckoId string) (osmomath.BigDec, error) {
 	if coingeckoId == "" {
 		return zeroBigDec, nil
