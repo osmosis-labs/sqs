827efc8a19ef6f0dd8bea558671d09c771138217
Roman <roman@osmosis.team>
2024-05-24 01:48:40 +0000

    reduce duplication

--- tests/test_router_quote.py
+++ tests/test_router_quote.py
@@ -62,40 +62,7 @@ class TestQuote:
         # Run the quote test
         quote = self.run_quote_test(environment_url, token_in_coin, denom_out, expected_latency_upper_bound_ms)
 
-        # Validate routes are generally present
-        assert len(quote.route) > 0
-
-        # Check if the route is a single pool single transmuter route
-        # For such routes, the price impact is 0.
-        is_transmuter_route = self.is_transmuter_in_single_route(quote.route)
-
-        # Validate price impact
-        # If it is a single pool single transmuter route, we expect the price impact to be 0
-        # Price impact is returned as a negative number for any other route.
-        assert quote.price_impact is not None
-        assert (not is_transmuter_route) and (quote.price_impact < 0) or (is_transmuter_route) and (quote.price_impact == 0), f"Error: price impact {quote.price_impact} is zero for non-transmuter route"
-        price_impact_positive = quote.price_impact * -1
-
-        # Validate amount in and denom are as input
-        assert quote.amount_in.amount == int(amount_str)
-        assert quote.amount_in.denom == USDC
-
-        # Validate that the fee is charged
-        assert quote.effective_fee > 0
-
-        # Validate that the spot price is present
-        assert quote.in_base_out_quote_spot_price is not None
-
-        # Validate that the spot price is within the error tolerance
-        assert relative_error(quote.in_base_out_quote_spot_price * spot_price_scaling_factor, expected_in_base_out_quote_price) < error_tolerance, f"Error: in base out quote spot price {quote.in_base_out_quote_spot_price} is not within {error_tolerance} of expected {expected_in_base_out_quote_price}"
-
-        # If there is a price impact greater than the provided error tolerance, we dynamically set the error tolerance to be
-        # the price impact * (1 + error_tolerance) to account for the price impact
-        if price_impact_positive > error_tolerance:
-            error_tolerance = price_impact_positive * Decimal(1 + error_tolerance)
-
-        # Validate that the amount out is within the error tolerance
-        assert relative_error(quote.amount_out * spot_price_scaling_factor, expected_token_out) < error_tolerance, f"Error: amount out {quote.amount_out} is not within {error_tolerance} of expected {expected_token_out}"
+        self.validate_quote_test(quote, amount_str, USDC, spot_price_scaling_factor, expected_in_base_out_quote_price, expected_token_out, error_tolerance)
 
     # - Constructrs combinations between each from 10^6 to 10^9 amount input
     @pytest.mark.parametrize("swap_pair", conftest.create_coins_from_pairs(conftest.create_no_dupl_token_pairs(conftest.choose_tokens_liq_range(num_tokens=10, min_liq=500_000, exponent_filter=USDC_PRECISION)), USDC_PRECISION, USDC_PRECISION + 3), ids=id_from_swap_pair)
@@ -104,10 +71,11 @@ class TestQuote:
         amount_str = token_in_obj['amount_str']
         token_in_denom = token_in_obj['denom']
         token_in_coin = amount_str + token_in_denom
-
         denom_out = swap_pair['out_denom']
 
-        # All tokens have the same exponent.
+        error_tolerance = 0.05
+
+        # All tokens have the same default exponent, resulting in scaling factor of 1.
         spot_price_scaling_factor = 1
 
         # Compute expected base out quote spot price
@@ -116,51 +84,13 @@ class TestQuote:
         in_base_usd_quote_price = conftest.get_usd_price_scaled(token_in_denom)
         expected_in_base_out_quote_price = in_base_usd_quote_price / out_base_usd_quote_price 
 
-        error_tolerance = 0.05
-
         # Compute expected token out
         expected_token_out = int(amount_str) * expected_in_base_out_quote_price
 
         # Run the quote test
         quote = self.run_quote_test(environment_url, token_in_coin, denom_out, expected_latency_upper_bound_ms)
 
-        # Validate routes are generally present
-        assert len(quote.route) > 0
-
-                # Validate routes are generally present
-        assert len(quote.route) > 0
-
-        # Check if the route is a single pool single transmuter route
-        # For such routes, the price impact is 0.
-        is_transmuter_route = self.is_transmuter_in_single_route(quote.route)
-
-        # Validate price impact
-        # If it is a single pool single transmuter route, we expect the price impact to be 0
-        # Price impact is returned as a negative number for any other route.
-        assert quote.price_impact is not None
-        assert (not is_transmuter_route) and (quote.price_impact < 0) or (is_transmuter_route) and (quote.price_impact == 0), f"Error: price impact {quote.price_impact} is zero for non-transmuter route"
-        price_impact_positive = quote.price_impact * -1
-
-        # Validate amount in and denom are as input
-        assert quote.amount_in.amount == int(amount_str)
-        assert quote.amount_in.denom == token_in_denom
-
-        # Validate that the fee is charged
-        assert quote.effective_fee > 0
-
-        # Validate that the spot price is present
-        assert quote.in_base_out_quote_spot_price is not None
-
-        # Validate that the spot price is within the error tolerance
-        assert relative_error(quote.in_base_out_quote_spot_price * spot_price_scaling_factor, expected_in_base_out_quote_price) < error_tolerance, f"Error: in base out quote spot price {quote.in_base_out_quote_spot_price} is not within {error_tolerance} of expected {expected_in_base_out_quote_price}"
-
-        # If there is a price impact greater than the provided error tolerance, we dynamically set the error tolerance to be
-        # the price impact * (1 + error_tolerance) to account for the price impact
-        if price_impact_positive > error_tolerance:
-            error_tolerance = price_impact_positive * Decimal(1 + error_tolerance)
-
-        # Validate that the amount out is within the error tolerance
-        assert relative_error(quote.amount_out * spot_price_scaling_factor, expected_token_out) < error_tolerance, f"Error: amount out {quote.amount_out} is not within {error_tolerance} of expected {expected_token_out}"
+        self.validate_quote_test(quote, amount_str, token_in_denom, spot_price_scaling_factor, expected_in_base_out_quote_price, expected_token_out, error_tolerance)
 
     ## TODO:
     # 18 exponent
@@ -193,6 +123,42 @@ class TestQuote:
 
         # Return route for more detailed validation
         return QuoteResponse(**response_json)
+
+    def validate_quote_test(self, quote, expected_amount_in_str, expected_denom_in, spot_price_scaling_factor, expected_in_base_out_quote_price, expected_token_out, error_tolerance):
+                # Validate routes are generally present
+        assert len(quote.route) > 0
+
+        # Check if the route is a single pool single transmuter route
+        # For such routes, the price impact is 0.
+        is_transmuter_route = self.is_transmuter_in_single_route(quote.route)
+
+        # Validate price impact
+        # If it is a single pool single transmuter route, we expect the price impact to be 0
+        # Price impact is returned as a negative number for any other route.
+        assert quote.price_impact is not None
+        assert (not is_transmuter_route) and (quote.price_impact < 0) or (is_transmuter_route) and (quote.price_impact == 0), f"Error: price impact {quote.price_impact} is zero for non-transmuter route"
+        price_impact_positive = quote.price_impact * -1
+
+        # Validate amount in and denom are as input
+        assert quote.amount_in.amount == int(expected_amount_in_str)
+        assert quote.amount_in.denom == expected_denom_in
+
+        # Validate that the fee is charged
+        assert quote.effective_fee > 0
+
+        # Validate that the spot price is present
+        assert quote.in_base_out_quote_spot_price is not None
+
+        # Validate that the spot price is within the error tolerance
+        assert relative_error(quote.in_base_out_quote_spot_price * spot_price_scaling_factor, expected_in_base_out_quote_price) < error_tolerance, f"Error: in base out quote spot price {quote.in_base_out_quote_spot_price} is not within {error_tolerance} of expected {expected_in_base_out_quote_price}"
+
+        # If there is a price impact greater than the provided error tolerance, we dynamically set the error tolerance to be
+        # the price impact * (1 + error_tolerance) to account for the price impact
+        if price_impact_positive > error_tolerance:
+            error_tolerance = price_impact_positive * Decimal(1 + error_tolerance)
+
+        # Validate that the amount out is within the error tolerance
+        assert relative_error(quote.amount_out * spot_price_scaling_factor, expected_token_out) < error_tolerance, f"Error: amount out {quote.amount_out} is not within {error_tolerance} of expected {expected_token_out}"
 
     def is_transmuter_in_single_route(self, routes):
         """
