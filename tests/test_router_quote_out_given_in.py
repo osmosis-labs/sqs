c741adeedcefbe4ae43cdbe4a5f07b04407df9c8
Deividas Petraitis <hi@deividaspetraitis.lt>
2024-07-26 14:15:12 +0300

    Feat: swap Exact Amount Out, SQS Routing and API changes
    Requested changes

--- tests/test_router_quote_out_given_in.py
+++ tests/test_router_quote_out_given_in.py
@@ -27,92 +27,91 @@ HIGH_LIQ_PRICE_IMPACT_CHECK_USD_AMOUNT_IN_THRESHOLD = 5000
 # The max price impact threshold for the high liquidity check
 HIGH_LIQ_MAX_PRICE_IMPACT_THRESHOLD = 0.5
 
+
 # Test suite for the /router/quote endpoint
-# Test runs tests for exact amount out quotes.
-class TestExactAmountOutQuote:
+# Test runs tests for exact amount in quotes.
+class TestExactAmountInQuote:
     @pytest.mark.parametrize("coin_obj", construct_token_in_combos(conftest.choose_tokens_liq_range(QUOTE_NUM_TOP_LIQUIDITY_DENOMS), USDC_PRECISION - 1, USDC_PRECISION + 4), ids=id_from_coin)
-    def test_usdc_in_high_liq_in(self, environment_url, coin_obj):
+    def test_usdc_in_high_liq_out(self, environment_url, coin_obj):
         """
-        This test case validates quotes between USDC in and NUM_TOP_LIQUIDITY_DENOMS.
+        This test case validates quotes betwen USDC in and NUM_TOP_LIQUIDITY_DENOMS.
         The amounts are constructed to be seeded random values between 10^USDC_PRECISION-1 and 10 ^(USDC_PRECISION + 4)
 
-        This allows us to validate that we can continue to quote at reasonable USDC values for all major token pairs without errors.
+        This allows us to validate that we can continue to quote at reasonable USDC values for all majore token pairs without errors.
 
         Note: the reason we use Decimal in this test is because floats truncate in some edge cases, leading
         to flakiness.
         """
 
-        denom_in = coin_obj["denom"]
+        denom_out = coin_obj["denom"]
         amount_str = coin_obj["amount_str"]
-        amount_out = int(amount_str)
+        amount_in = int(amount_str)
 
-        # Choose the error tolerance based on amount in swapped.
-        error_tolerance = Quote.choose_error_tolerance(amount_out)
+        # Choosse the error tolerance based on amount in swapped.
+        error_tolerance = Quote.choose_error_tolerance(amount_in)
 
         # Skip USDC quotes
-        if denom_in == USDC:
+        if denom_out == USDC:
             return
 
-        denom_in_data = conftest.shared_test_state.chain_denom_to_data_map.get(denom_in)
-        denom_in_precision = denom_in_data.get("exponent")
+        denom_out_data = conftest.shared_test_state.chain_denom_to_data_map.get(denom_out)
+        denom_out_precision = denom_out_data.get("exponent")
         
         # Compute spot price scaling factor.
-        spot_price_scaling_factor = Decimal(10)**6 / Decimal(10)**denom_in_precision
+        spot_price_scaling_factor = Decimal(10)**6 / Decimal(10)**denom_out_precision
 
         # Compute expected spot prices
-        out_base_in_quote_price = Decimal(denom_in_data.get("price"))
-        expected_out_base_in_quote_price = 1 / out_base_in_quote_price
+        out_base_in_quote_price = Decimal(denom_out_data.get("price"))
+        expected_in_base_out_quote_price = 1 / out_base_in_quote_price
         
         # Compute expected token out
-        expected_token_out = int(amount_str) * expected_out_base_in_quote_price
+        expected_token_out = int(amount_str) * expected_in_base_out_quote_price
 
         # Set the token in coin
-        token_out_coin = amount_str + USDC
+        token_in_coin = amount_str + USDC
 
         # Run the quote test
-        quote = self.run_quote_test(environment_url, token_out_coin, denom_in, EXPECTED_LATENCY_UPPER_BOUND_MS)
-
-        self.validate_quote_test(quote, amount_str, USDC, spot_price_scaling_factor, expected_out_base_in_quote_price, expected_token_out, error_tolerance)
+        quote = self.run_quote_test(environment_url, token_in_coin, denom_out, EXPECTED_LATENCY_UPPER_BOUND_MS)
+        self.validate_quote_test(quote, amount_str, USDC, spot_price_scaling_factor, expected_in_base_out_quote_price, expected_token_out, error_tolerance)
 
     # - Constructs combinations between each from 10^6 to 10^9 amount input
     @pytest.mark.parametrize("swap_pair", conftest.create_coins_from_pairs(conftest.create_no_dupl_token_pairs(conftest.choose_tokens_liq_range(num_tokens=10, min_liq=500_000, exponent_filter=USDC_PRECISION)), USDC_PRECISION, USDC_PRECISION + 3), ids=id_from_swap_pair)
     def test_top_liq_combos_default_exponent(self, environment_url, swap_pair):
-        token_out_obj = swap_pair['token_in']
-        amount_str = token_out_obj['amount_str']
-        token_out_denom = token_out_obj['denom']
-        token_out_coin = amount_str + token_out_denom
-        denom_in = swap_pair['out_denom']
-        amount_out = int(amount_str)
-
+        token_in_obj = swap_pair['token_in']
+        amount_str = token_in_obj['amount_str']
+        token_in_denom = token_in_obj['denom']
+        token_in_coin = amount_str + token_in_denom
+        denom_out = swap_pair['out_denom']
+        amount_in = int(amount_str)
 
         # All tokens have the same default exponent, resulting in scaling factor of 1.
         spot_price_scaling_factor = 1
 
         # Compute expected base out quote spot price
         # First, get the USD price of each denom, and then divide to get the expected spot price
-        in_base_usd_quote_price = conftest.get_usd_price_scaled(denom_in)
-        out_base_usd_quote_price = conftest.get_usd_price_scaled(token_out_denom)
-        expected_out_base_in_quote_price = out_base_usd_quote_price / in_base_usd_quote_price 
+        out_base_usd_quote_price = conftest.get_usd_price_scaled(denom_out)
+        in_base_usd_quote_price = conftest.get_usd_price_scaled(token_in_denom)
+        expected_in_base_out_quote_price = in_base_usd_quote_price / out_base_usd_quote_price 
 
         # Compute expected token out
-        expected_token_in = int(amount_str) * expected_out_base_in_quote_price
+        expected_token_out = int(amount_str) * expected_in_base_out_quote_price
 
-        token_out_amount_usdc_value = in_base_usd_quote_price * amount_out
+        token_in_amount_usdc_value = in_base_usd_quote_price * amount_in
 
-        # Chose the error tolerance based on amount in swapped.
-        error_tolerance = Quote.choose_error_tolerance(token_out_amount_usdc_value)
+        # Choosse the error tolerance based on amount in swapped.
+        error_tolerance = Quote.choose_error_tolerance(token_in_amount_usdc_value)
 
         # Run the quote test
-        quote = self.run_quote_test(environment_url, token_out_coin, denom_in, EXPECTED_LATENCY_UPPER_BOUND_MS)
+        quote = self.run_quote_test(environment_url, token_in_coin, denom_out, EXPECTED_LATENCY_UPPER_BOUND_MS)
         # Validate that price impact is present.
         assert quote.price_impact is not None
 
         # If the token in amount value is less than $HIGH_LIQ_PRICE_IMPACT_CHECK_USD_AMOUNT_IN_THRESHOLD, we expect the price impact to not exceed threshold
-        if token_out_amount_usdc_value < HIGH_LIQ_PRICE_IMPACT_CHECK_USD_AMOUNT_IN_THRESHOLD:
+        if token_in_amount_usdc_value < HIGH_LIQ_PRICE_IMPACT_CHECK_USD_AMOUNT_IN_THRESHOLD:
              quote.price_impact * -1 < HIGH_LIQ_MAX_PRICE_IMPACT_THRESHOLD, f"Error: price impact is either None or greater than {HIGH_LIQ_MAX_PRICE_IMPACT_THRESHOLD} {quote.price_impact}"
 
         # Validate quote results
-        self.validate_quote_test(quote, amount_str, token_out_denom, spot_price_scaling_factor, expected_out_base_in_quote_price, expected_token_in, error_tolerance)
+        self.validate_quote_test(quote, amount_str, token_in_denom, spot_price_scaling_factor, expected_in_base_out_quote_price, expected_token_out, error_tolerance)
 
     @pytest.mark.parametrize("amount", [str(10**(USDC_PRECISION + 3))])
     def test_transmuter_tokens(self, environment_url, amount):
@@ -138,41 +137,42 @@ class TestExactAmountOutQuote:
 
         transmuter_token_pair = transmuter_token_data[1]
 
-        denom_in = transmuter_token_pair[1]
-        denom_out = transmuter_token_pair[0]
+        denom_in = transmuter_token_pair[0]
+        denom_out = transmuter_token_pair[1]
 
         # This is the max error tolerance of 5% that we allow.
         # Arbitrarily hand-picked to avoid flakiness.
         error_tolerance = 0.05
 
         # Get denom in precision.
-        denom_out_precision = conftest.get_denom_exponent(denom_out)
+        denom_in_precision = conftest.get_denom_exponent(denom_in)
 
         # Get denom out data to retrieve precision and price 
-        denom_in_data = conftest.shared_test_state.chain_denom_to_data_map.get(denom_in)
-        denom_in_precision = denom_in_data.get("exponent")
+        denom_out_data = conftest.shared_test_state.chain_denom_to_data_map.get(denom_out)
+        denom_out_precision = denom_out_data.get("exponent")
         
         # Compute spot price scaling factor.
-        spot_price_scaling_factor = Decimal(10)**denom_out_precision / Decimal(10)**denom_in_precision
+        spot_price_scaling_factor = Decimal(10)**denom_in_precision / Decimal(10)**denom_out_precision
 
         # Compute expected spot prices
-        out_base_in_quote_price = Decimal(denom_in_data.get("price"))
-        expected_out_base_in_quote_price = 1 / out_base_in_quote_price
+        out_base_in_quote_price = Decimal(denom_out_data.get("price"))
+        expected_in_base_out_quote_price = 1 / out_base_in_quote_price
         
         # Compute expected token out
-        expected_token_in = int(amount) * expected_out_base_in_quote_price
+        expected_token_out = int(amount) * expected_in_base_out_quote_price
 
         # Run the quote test
-        quote = self.run_quote_test(environment_url, amount + denom_out, denom_in, EXPECTED_LATENCY_UPPER_BOUND_MS)
+        quote = self.run_quote_test(environment_url, amount + denom_in, denom_out, EXPECTED_LATENCY_UPPER_BOUND_MS)
 
         # Validate transmuter was in route
         assert Quote.is_transmuter_in_single_route(quote.route) is True
 
         # Validate the quote test
-        self.validate_quote_test(quote, amount, denom_out, spot_price_scaling_factor, expected_out_base_in_quote_price, expected_token_in, error_tolerance)
-    def run_quote_test(self, environment_url, token_out, token_in, expected_latency_upper_bound_ms, expected_status_code=200) -> QuoteExactAmountOutResponse:
+        self.validate_quote_test(quote, amount, denom_in, spot_price_scaling_factor, expected_in_base_out_quote_price, expected_token_out, error_tolerance)
+
+    def run_quote_test(self, environment_url, token_in, token_out, expected_latency_upper_bound_ms, expected_status_code=200) -> QuoteExactAmountInResponse:
         """
-        Runs exact amount out test for the /router/quote endpoint with the given input parameters.
+        Runs a test for the /router/quote endpoint with the given input parameters.
 
         Does basic validation around response status code and latency
 
@@ -182,11 +182,11 @@ class TestExactAmountOutQuote:
         - Response status code is as given or default 200
         - Latency is under the given bound
         """
-
+        
         sqs_service = conftest.SERVICE_MAP[environment_url]
 
         start_time = time.time()
-        response = sqs_service.get_exact_amount_out_quote(token_out, token_in)
+        response = sqs_service.get_exact_amount_in_quote(token_in, token_out)
         elapsed_time_ms = (time.time() - start_time) * 1000
 
         assert response.status_code == expected_status_code, f"Error: {response.text}"
@@ -195,9 +195,9 @@ class TestExactAmountOutQuote:
         response_json = response.json()
 
         # Return route for more detailed validation
-        return QuoteExactAmountOutResponse(**response_json)
+        return QuoteExactAmountInResponse(**response_json)
 
-    def validate_quote_test(self, quote, expected_amount_out_str, expected_denom_out, spot_price_scaling_factor, expected_out_base_in_quote_price, expected_token_in, error_tolerance):
+    def validate_quote_test(self, quote, expected_amount_in_str, expected_denom_in, spot_price_scaling_factor, expected_in_base_out_quote_price, expected_token_out, error_tolerance):
         """
         Runs the following validations:
         - Basic presence of fields
@@ -221,8 +221,8 @@ class TestExactAmountOutQuote:
         price_impact_positive = quote.price_impact * -1
 
         # Validate amount in and denom are as input
-        assert quote.amount_out.amount == int(expected_amount_out_str)
-        assert quote.amount_out.denom == expected_denom_out
+        assert quote.amount_in.amount == int(expected_amount_in_str)
+        assert quote.amount_in.denom == expected_denom_in
 
         # Validate that the fee is charged
         Quote.validate_fee(quote)
@@ -231,7 +231,7 @@ class TestExactAmountOutQuote:
         assert quote.in_base_out_quote_spot_price is not None
 
         # Validate that the spot price is within the error tolerance
-        assert relative_error(quote.in_base_out_quote_spot_price * spot_price_scaling_factor, expected_out_base_in_quote_price) < error_tolerance, f"Error: in base out quote spot price {quote.in_base_out_quote_spot_price} is not within {error_tolerance} of expected {expected_out_base_in_quote_price}"
+        assert relative_error(quote.in_base_out_quote_spot_price * spot_price_scaling_factor, expected_in_base_out_quote_price) < error_tolerance, f"Error: in base out quote spot price {quote.in_base_out_quote_spot_price} is not within {error_tolerance} of expected {expected_in_base_out_quote_price}"
 
         # If there is a price impact greater than the provided error tolerance, we dynamically set the error tolerance to be
         # the price impact * (1 + error_tolerance) to account for the price impact
@@ -239,5 +239,5 @@ class TestExactAmountOutQuote:
             error_tolerance = price_impact_positive * Decimal(1 + error_tolerance)
 
         # Validate that the amount out is within the error tolerance
-        amount_in_scaled = quote.amount_in * spot_price_scaling_factor
-        assert relative_error(amount_in_scaled, expected_token_in) < error_tolerance, f"Error: amount out scaled {amount_out_scaled} is not within {error_tolerance} of expected {expected_token_out}"
+        amount_out_scaled = quote.amount_out * spot_price_scaling_factor
+        assert relative_error(amount_out_scaled, expected_token_out) < error_tolerance, f"Error: amount out scaled {amount_out_scaled} is not within {error_tolerance} of expected {expected_token_out}"
