939574491d5b0345f19ce4e69fb005779392f727
Deividas Petraitis <hi@deividaspetraitis.lt>
2024-07-26 16:18:59 +0300

    Feat: swap Exact Amount Out, SQS Routing and API changes
    Split tests cases

---
+++ locust/in_given_out.py
@@ -0,0 +1,54 @@
+from locust import HttpUser, task
+from pairs import *
+
+class ExactAmountOutQuote(HttpUser):
+    # on_start is called when a Locust start before any task is scheduled.
+    def on_start(self):
+        pass
+
+    # on_stop is called when the TaskSet is stopping
+    def on_stop(self):
+        pass
+
+    # Quote the same pair of UOSMO and USDC (UOSMO in) while progressively
+    # increasing the amount of the tokenIn per endpoint.
+    #
+    # Token swap method is exact amount out.
+    @task
+    def quoteUOSMOUSDC_1In(self):
+        self.client.get(f"/router/quote?tokenOut=1000000{UOSMO}&tokenInDenom={USDC}")
+
+    @task
+    def quoteUOSMOUSDC_1000In(self):
+        self.client.get(f"/router/quote?tokenOut=1000000000{UOSMO}&tokenInDenom={USDC}")
+
+    @task
+    def quoteUOSMOUSDC_1000000In(self):
+        self.client.get(f"/router/quote?tokenOut=1000000000000{UOSMO}&tokenInDenom={USDC}")
+
+    # Quote the same pair of UOSMO and USDC (USDC in).
+    # Token swap method is exact amount out.
+    @task
+    def quoteUSDCUOSMO_1000000In(self):
+        self.client.get(f"/router/quote?tokenOut=100000000000{USDC}&tokenInDenom={UOSMO}")
+
+    @task
+    def quoteUSDCTUMEE_3000IN(self):
+        self.client.get(f"/router/quote?tokenOut=3000000000{USDT}&tokenInDenom={UMEE}")
+
+    @task
+    def quoteASTROCWPool(self):
+        self.client.get(f"/router/quote?tokenOut=1000000000{UOSMO}&tokenInDenom={ASTRO}")
+
+    @task
+    def quoteInvalidToken(self):
+        self.client.get(f"/router/quote?tokenOut=1000000000{UOSMO}&tokenInDenom={INVALID_DENOM}")
+
+    @task
+    def routesUOSMOUSDC(self):
+        self.client.get(f"/router/routes?tokenOut={UOSMO}&tokenInDenom={USDC}")
+
+    @task
+    def routesUSDCUOSMO(self):
+        self.client.get(f"/router/routes?tokenOut={USDC}&tokenInDenom={UOSMO}")
+
