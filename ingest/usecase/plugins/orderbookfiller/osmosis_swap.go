c9d9e5bf9aaaf7349aa92f799812c65305dd1d6f
Adam Tucker <adamleetucker@outlook.com>
2024-07-28 19:04:52 -0600

    add swap exact amount in

---
+++ ingest/usecase/plugins/orderbookfiller/osmosis_swap.go
@@ -0,0 +1,254 @@
+package orderbookfiller
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	"github.com/osmosis-labs/sqs/domain/keyring"
+
+	cometrpc "github.com/cometbft/cometbft/rpc/client/http"
+	coretypes "github.com/cometbft/cometbft/rpc/core/types"
+	tmtypes "github.com/cometbft/cometbft/types"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/tx/signing"
+	authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
+	"github.com/cosmos/ibc-go/v7/testing/simapp"
+	poolmanager "github.com/osmosis-labs/osmosis/v25/x/poolmanager/module"
+	poolmanagertypes "github.com/osmosis-labs/osmosis/v25/x/poolmanager/types"
+)
+
+// var cdc = codec.NewProtoCodec(codectypes.NewInterfaceRegistry())
+
+var (
+	RPC   = "http://127.0.0.1:26657"
+	LCD   = "http://127.0.0.1:1317"
+	Denom = "uosmo"
+)
+
+type AccountInfo struct {
+	Sequence      string `json:"sequence"`
+	AccountNumber string `json:"account_number"`
+}
+
+type AccountResult struct {
+	Account AccountInfo `json:"account"`
+}
+
+func getInitialSequence(address string) (uint64, uint64) {
+	resp, err := httpGet(LCD + "/cosmos/auth/v1beta1/accounts/" + address)
+	if err != nil {
+		log.Printf("Failed to get initial sequence: %v", err)
+		return 0, 0
+	}
+
+	var accountRes AccountResult
+	err = json.Unmarshal(resp, &accountRes)
+	if err != nil {
+		log.Printf("Failed to unmarshal account result: %v", err)
+		return 0, 0
+	}
+
+	seqint, err := strconv.ParseUint(accountRes.Account.Sequence, 10, 64)
+	if err != nil {
+		log.Printf("Failed to convert sequence to int: %v", err)
+		return 0, 0
+	}
+
+	accnum, err := strconv.ParseUint(accountRes.Account.AccountNumber, 10, 64)
+	if err != nil {
+		log.Printf("Failed to convert account number to int: %v", err)
+		return 0, 0
+	}
+
+	return seqint, accnum
+}
+
+var client = &http.Client{
+	Timeout: 10 * time.Second, // Adjusted timeout to 10 seconds
+	Transport: &http.Transport{
+		MaxIdleConns:        100,              // Increased maximum idle connections
+		MaxIdleConnsPerHost: 10,               // Increased maximum idle connections per host
+		IdleConnTimeout:     90 * time.Second, // Increased idle connection timeout
+		TLSHandshakeTimeout: 10 * time.Second, // Increased TLS handshake timeout
+	},
+}
+
+func httpGet(url string) ([]byte, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		netErr, ok := err.(net.Error)
+		if ok && netErr.Timeout() {
+			log.Printf("Request to %s timed out, continuing...", url)
+			return nil, nil
+		}
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
+
+func (o *orderbookFillerIngestPlugin) swapExactAmountIn(tokenIn sdk.Coin, sequence, accnum uint64, keyring keyring.Keyring) (response *coretypes.ResultBroadcastTx, txbody string, err error) {
+	chainID := "osmosis-1"
+	o.swapDone.Store(true)
+
+	key := keyring.GetKey()
+	keyBytes := key.Bytes()
+
+	privKey, err := newSecp256k1PrivKeyFromBytes(keyBytes)
+	if err != nil {
+		return nil, "", err
+	}
+
+	encodingConfig := simapp.MakeTestEncodingConfig()
+	// encodingConfig.Marshaler = cdc
+
+	// Register types
+	poolm := poolmanager.AppModuleBasic{}
+	poolm.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+
+	// Create a new TxBuilder.
+	txBuilder := encodingConfig.TxConfig.NewTxBuilder()
+
+	// TODO: Unhardcode route
+	route := []poolmanagertypes.SwapAmountInRoute{
+		{
+			PoolId:        1,
+			TokenOutDenom: "ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2",
+		},
+	}
+
+	swapMsg := &poolmanagertypes.MsgSwapExactAmountIn{
+		Sender:            keyring.GetAddress().String(),
+		Routes:            route,
+		TokenIn:           tokenIn,
+		TokenOutMinAmount: sdk.OneInt(),
+	}
+
+	msg := []sdk.Msg{swapMsg}
+
+	// set messages
+	err = txBuilder.SetMsgs(msg...)
+	if err != nil {
+		return nil, "", err
+	}
+
+	// // Estimate gas limit based on transaction size
+	// txSize := 0
+	// for _, m := range msg {
+	// 	txSize += m.Size()
+	// }
+	// txSize := msg
+	// gasLimit := uint64((txSize * config.Bytes) + config.BaseGas)
+	gasLimit := uint64(1700000)
+	txBuilder.SetGasLimit(gasLimit)
+
+	// Calculate fee based on gas limit and a fixed gas price
+	gasPrice := sdk.NewDecCoinFromDec(Denom, sdk.NewDecWithPrec(25, 4)) // 0.00051 token per gas unit
+	feeAmount := gasPrice.Amount.MulInt64(int64(gasLimit)).RoundInt()
+	feecoin := sdk.NewCoin(Denom, feeAmount)
+	txBuilder.SetFeeAmount(sdk.NewCoins(feecoin))
+	txBuilder.SetTimeoutHeight(0)
+
+	// First round: we gather all the signer infos. We use the "set empty
+	// signature" hack to do that.
+	sigV2 := signing.SignatureV2{
+		PubKey: privKey.PubKey(),
+		Data: &signing.SingleSignatureData{
+			SignMode:  encodingConfig.TxConfig.SignModeHandler().DefaultMode(),
+			Signature: nil,
+		},
+		Sequence: sequence,
+	}
+
+	err = txBuilder.SetSignatures(sigV2)
+	if err != nil {
+		fmt.Println("error setting signatures")
+		return nil, "", err
+	}
+
+	signerData := authsigning.SignerData{
+		ChainID:       chainID,
+		AccountNumber: accnum,
+		Sequence:      sequence,
+	}
+
+	signed, err := tx.SignWithPrivKey(
+		encodingConfig.TxConfig.SignModeHandler().DefaultMode(), signerData,
+		txBuilder, privKey, encodingConfig.TxConfig, sequence)
+	if err != nil {
+		fmt.Println("couldn't sign")
+		return nil, "", err
+	}
+
+	err = txBuilder.SetSignatures(signed)
+	if err != nil {
+		return nil, "", err
+	}
+
+	// Generate a JSON string.
+	txJSONBytes, err := encodingConfig.TxConfig.TxEncoder()(txBuilder.GetTx())
+	if err != nil {
+		fmt.Println(err)
+		return nil, "", err
+	}
+
+	resp, err := BroadcastTransaction(txJSONBytes, RPC)
+	if err != nil {
+		return nil, "", fmt.Errorf("failed to broadcast transaction: %w", err)
+	}
+
+	return resp, string(txJSONBytes), nil
+}
+
+func BroadcastTransaction(txBytes []byte, rpcEndpoint string) (*coretypes.ResultBroadcastTx, error) {
+	cmtCli, err := cometrpc.New(rpcEndpoint, "/websocket")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	t := tmtypes.Tx(txBytes)
+
+	ctx := context.Background()
+	res, err := cmtCli.BroadcastTxSync(ctx, t)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("error at broadcast")
+		return nil, err
+	}
+
+	fmt.Println("other: ", res.Data)
+	fmt.Println("log: ", res.Log)
+	fmt.Println("code: ", res.Code)
+	fmt.Println("code: ", res.Codespace)
+	fmt.Println("txid: ", res.Hash)
+
+	return res, nil
+}
+
+func newSecp256k1PrivKeyFromBytes(privKeyBytes []byte) (cryptotypes.PrivKey, error) {
+	privKey := &secp256k1.PrivKey{Key: privKeyBytes}
+	return privKey, nil
+}
