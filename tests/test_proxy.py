1b78ed197628cfa6aa7897477aa57985f5201658
yury-dubinin <yury.dubinin@gmail.com>
2024-08-02 12:18:45 +0200

    comment from AI

--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -4,5 +4,6 @@ from unittest import TestCase
 
 class ProxyTesting(TestCase):
     def test_proxy(self):
-        r = requests.get("https://ipinfo.io")
-        print(r)
+        response = requests.get("https://ipinfo.io")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("ip", response.json())
