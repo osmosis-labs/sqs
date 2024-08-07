5c8eae457ec90eeb6823a88b08d65629dbc8b94b
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2024-08-07 15:46:44 +0700

    add number conversion test from rust side

--- rustffi/rustsrc/src/numbers.rs
+++ rustffi/rustsrc/src/numbers.rs
@@ -24,3 +24,36 @@ impl From<FFIDecimal> for transmuter_math::Decimal {
         transmuter_math::Decimal::raw(value.0.into())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ffiu128_conversion() {
+        // Test small number
+        let small: u128 = 42;
+        let ffi_small = FFIU128::from(small);
+        assert_eq!(u128::from(ffi_small), small);
+
+        // Test large number
+        let large: u128 = 0xFFFFFFFFFFFFFFFF_FFFFFFFFFFFFFFFF;
+        let ffi_large = FFIU128::from(large);
+        assert_eq!(u128::from(ffi_large), large);
+
+        // Test middle range number
+        let middle: u128 = 0x0123456789ABCDEF_0123456789ABCDEF;
+        let ffi_middle = FFIU128::from(middle);
+        assert_eq!(u128::from(ffi_middle), middle);
+
+        // Test zero
+        let zero: u128 = 0;
+        let ffi_zero = FFIU128::from(zero);
+        assert_eq!(u128::from(ffi_zero), zero);
+
+        // Test max u64 + 1
+        let over_u64: u128 = 0x0000000000000001_0000000000000000;
+        let ffi_over_u64 = FFIU128::from(over_u64);
+        assert_eq!(u128::from(ffi_over_u64), over_u64);
+    }
+}
