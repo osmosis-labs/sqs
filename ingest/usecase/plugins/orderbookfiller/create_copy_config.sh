96d12a7229ae86f3aa2bf9b1bb5e0b9a45f51daa
Roman <roman@osmosis.team>
2024-08-11 21:51:31 +0000

    modify `app.toml` for start script

--- ingest/usecase/plugins/orderbookfiller/create_copy_config.sh
+++ ingest/usecase/plugins/orderbookfiller/create_copy_config.sh
@@ -13,3 +13,17 @@ jq '.
     "$INPUT_FILE" > "$OUTPUT_FILE"
 
 echo "Modified configuration saved to $OUTPUT_FILE"
+
+# Define the input and output file paths
+ORIGINAL_APP_TOML_NAME="$HOME/.osmosisd/config/app.toml"   # Replace with the actual file path
+BACKUP_APP_TOML_NAME="$HOME/.osmosisd/config/app-backup.toml"
+
+mv $ORIGINAL_APP_TOML_NAME $BACKUP_APP_TOML_NAME
+
+# Use sed to modify the TOML and create a new file
+sed -e 's/^service-name = "osmosis-dev"/service-name = "osmosis-fill-bot"/' \
+    -e 's/^grpc-ingest-address = "localhost:50051"/grpc-ingest-address = "osmosis-sqs:50051"/' \
+    -e '/^\[osmosis-sqs\]/,/^is-enabled = ".*"/s/^is-enabled = ".*"/is-enabled = "true"/' \
+    "$BACKUP_APP_TOML_NAME" > "$ORIGINAL_APP_TOML_NAME"
+
+echo "Modified configuration saved to $ORIGINAL_APP_TOML_NAME, backup made in $BACKUP_APP_TOML_NAME"
