6f0f144677f2987856cb606ffc4c1a68042a40df
Roman <roman@osmosis.team>
2024-08-17 00:46:27 -0600

    Update tests/route.py

--- tests/route.py
+++ tests/route.py
@@ -10,7 +10,6 @@ def get_last_route_token_in(route):
         token_in_denom = pool.token_in_denom
     return token_in_denom
 
-
 def get_last_quote_route_token_in(quote):
     token_in_denom = ""
     for route in quote.route:
