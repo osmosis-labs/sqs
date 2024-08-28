78f4094de48fc4c45759e19c9b473d1cca8d480c
ghost <paddymchale@hotmail.com>
2024-08-28 10:09:22 +0100

    fix: rename tenderming to tendermint

--- app/sqs_config.go
+++ app/sqs_config.go
@@ -13,7 +13,7 @@ var DefaultConfig = domain.Config{
 	LoggerIsProduction: true,
 	LoggerLevel:        "info",
 
-	ChainTendermingRPCEndpoint: "http://localhost:26657",
+	ChainTendermintRPCEndpoint: "http://localhost:26657",
 	ChainGRPCGatewayEndpoint:   "http://localhost:9090",
 	ChainID:                    "osmosis-1",
 	ChainRegistryAssetsFileURL: "https://raw.githubusercontent.com/osmosis-labs/assetlists/main/osmosis-1/generated/frontend/assetlist.json",
