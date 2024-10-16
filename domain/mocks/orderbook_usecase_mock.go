ff24b7414f3f20c6621c0ff9c0303d3588814913
Deividas Petraitis <hi@deividaspetraitis.lt>
2024-10-16 09:01:57 +0300

    BE-594/CreateFormattedLimitOrder | OrderbookUsecase CreateFormattedLimitOrder method
    Refactor OrderbookUsecase to expose CreateFormattedLimitOrder as public
    method. That allows to reuse existing code for dealing with limit orders
    such as for example computing fill percentage of the orders.

--- domain/mocks/orderbook_usecase_mock.go
+++ domain/mocks/orderbook_usecase_mock.go
@@ -3,6 +3,7 @@ package mocks
 import (
 	"context"
 
+	"github.com/osmosis-labs/sqs/domain"
 	"github.com/osmosis-labs/sqs/domain/mvc"
 	orderbookdomain "github.com/osmosis-labs/sqs/domain/orderbook"
 	"github.com/osmosis-labs/sqs/sqsdomain"
@@ -12,10 +13,11 @@ var _ mvc.OrderBookUsecase = &OrderbookUsecaseMock{}
 
 // OrderbookUsecaseMock is a mock implementation of the RouterUsecase interface
 type OrderbookUsecaseMock struct {
-	ProcessPoolFunc           func(ctx context.Context, pool sqsdomain.PoolI) error
-	GetAllTicksFunc           func(poolID uint64) (map[int64]orderbookdomain.OrderbookTick, bool)
-	GetActiveOrdersFunc       func(ctx context.Context, address string) ([]orderbookdomain.LimitOrder, bool, error)
-	GetActiveOrdersStreamFunc func(ctx context.Context, address string) <-chan orderbookdomain.OrderbookResult
+	ProcessPoolFunc               func(ctx context.Context, pool sqsdomain.PoolI) error
+	GetAllTicksFunc               func(poolID uint64) (map[int64]orderbookdomain.OrderbookTick, bool)
+	GetActiveOrdersFunc           func(ctx context.Context, address string) ([]orderbookdomain.LimitOrder, bool, error)
+	GetActiveOrdersStreamFunc     func(ctx context.Context, address string) <-chan orderbookdomain.OrderbookResult
+	CreateFormattedLimitOrderFunc func(orderbook domain.CanonicalOrderBooksResult, order orderbookdomain.Order) (orderbookdomain.LimitOrder, error)
 }
 
 func (m *OrderbookUsecaseMock) ProcessPool(ctx context.Context, pool sqsdomain.PoolI) error {
@@ -42,6 +44,18 @@ func (m *OrderbookUsecaseMock) GetActiveOrders(ctx context.Context, address stri
 func (m *OrderbookUsecaseMock) GetActiveOrdersStream(ctx context.Context, address string) <-chan orderbookdomain.OrderbookResult {
 	if m.GetActiveOrdersStreamFunc != nil {
 		return m.GetActiveOrdersStreamFunc(ctx, address)
+	}
+	panic("unimplemented")
+}
+func (m *OrderbookUsecaseMock) WithCreateFormattedLimitOrder(order orderbookdomain.LimitOrder, err error) {
+	m.CreateFormattedLimitOrderFunc = func(domain.CanonicalOrderBooksResult, orderbookdomain.Order) (orderbookdomain.LimitOrder, error) {
+		return order, err
+	}
+}
+
+func (m *OrderbookUsecaseMock) CreateFormattedLimitOrder(orderbook domain.CanonicalOrderBooksResult, order orderbookdomain.Order) (orderbookdomain.LimitOrder, error) {
+	if m.CreateFormattedLimitOrderFunc != nil {
+		return m.CreateFormattedLimitOrderFunc(orderbook, order)
 	}
 	panic("unimplemented")
 }
