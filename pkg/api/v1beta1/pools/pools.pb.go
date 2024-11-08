9feff67bc8f9010559f1f5ca39a3d190aaa6f0f8
Deividas Petraitis <hi@deividaspetraitis.lt>
2024-11-08 18:02:55 +0200

    BE-636 | proto files, pools.pb.go regenerate

--- pkg/api/v1beta1/pools/pools.pb.go
+++ pkg/api/v1beta1/pools/pools.pb.go
@@ -37,8 +37,6 @@ type GetPoolsRequest struct {
 	// with_market_incentives indicates whether to include incentives into pools
 	// response.
 	WithMarketIncentives bool `protobuf:"varint,3,opt,name=with_market_incentives,json=withMarketIncentives,proto3" json:"with_market_incentives,omitempty"`
-	// Filtering options for querying pools
-	Filter *QueryFilter `protobuf:"bytes,4,opt,name=filter,proto3" json:"filter,omitempty"`
 	// Pagination options for the result set
 	Pagination *v1beta1.PaginationRequest `protobuf:"bytes,5,opt,name=pagination,proto3" json:"pagination,omitempty"`
 }
@@ -97,13 +95,6 @@ func (m *GetPoolsRequest) GetWithMarketIncentives() bool {
 	return false
 }
 
-func (m *GetPoolsRequest) GetFilter() *QueryFilter {
-	if m != nil {
-		return m.Filter
-	}
-	return nil
-}
-
 func (m *GetPoolsRequest) GetPagination() *v1beta1.PaginationRequest {
 	if m != nil {
 		return m.Pagination
@@ -111,105 +102,37 @@ func (m *GetPoolsRequest) GetPagination() *v1beta1.PaginationRequest {
 	return nil
 }
 
-// PoolFilter defines criteria to filter pools by specific attributes.
-type QueryFilter struct {
-	// A list of pool IDs to query; returns all pools if empty.
-	PoolIds []uint64 `protobuf:"varint,1,rep,packed,name=pool_ids,json=poolIds,proto3" json:"pool_ids,omitempty"`
-	// Minimum liquidity cap to filter pools by.
-	MinLiquidityCap uint64 `protobuf:"varint,2,opt,name=min_liquidity_cap,json=minLiquidityCap,proto3" json:"min_liquidity_cap,omitempty"`
-	// Indicates whether to include pools with market incentives.
-	WithMarketIncentives bool `protobuf:"varint,3,opt,name=with_market_incentives,json=withMarketIncentives,proto3" json:"with_market_incentives,omitempty"`
-}
-
-func (m *QueryFilter) Reset()         { *m = QueryFilter{} }
-func (m *QueryFilter) String() string { return proto.CompactTextString(m) }
-func (*QueryFilter) ProtoMessage()    {}
-func (*QueryFilter) Descriptor() ([]byte, []int) {
-	return fileDescriptor_30f2696e6c186971, []int{1}
-}
-func (m *QueryFilter) XXX_Unmarshal(b []byte) error {
-	return m.Unmarshal(b)
-}
-func (m *QueryFilter) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	if deterministic {
-		return xxx_messageInfo_QueryFilter.Marshal(b, m, deterministic)
-	} else {
-		b = b[:cap(b)]
-		n, err := m.MarshalToSizedBuffer(b)
-		if err != nil {
-			return nil, err
-		}
-		return b[:n], nil
-	}
-}
-func (m *QueryFilter) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_QueryFilter.Merge(m, src)
-}
-func (m *QueryFilter) XXX_Size() int {
-	return m.Size()
-}
-func (m *QueryFilter) XXX_DiscardUnknown() {
-	xxx_messageInfo_QueryFilter.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_QueryFilter proto.InternalMessageInfo
-
-func (m *QueryFilter) GetPoolIds() []uint64 {
-	if m != nil {
-		return m.PoolIds
-	}
-	return nil
-}
-
-func (m *QueryFilter) GetMinLiquidityCap() uint64 {
-	if m != nil {
-		return m.MinLiquidityCap
-	}
-	return 0
-}
-
-func (m *QueryFilter) GetWithMarketIncentives() bool {
-	if m != nil {
-		return m.WithMarketIncentives
-	}
-	return false
-}
-
 func init() {
 	proto.RegisterType((*GetPoolsRequest)(nil), "sqs.pools.v1beta1.GetPoolsRequest")
-	proto.RegisterType((*QueryFilter)(nil), "sqs.pools.v1beta1.QueryFilter")
 }
 
 func init() { proto.RegisterFile("sqs/pools/v1beta1/pools.proto", fileDescriptor_30f2696e6c186971) }
 
 var fileDescriptor_30f2696e6c186971 = []byte{
-	// 407 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xb4, 0x92, 0xc1, 0x6e, 0xd4, 0x30,
-	0x10, 0x86, 0xd7, 0xed, 0xb2, 0xad, 0xdc, 0x43, 0xb5, 0x51, 0x45, 0x43, 0x25, 0xac, 0x68, 0xc5,
-	0x21, 0x42, 0x22, 0x56, 0x00, 0xf1, 0x00, 0x80, 0x40, 0x15, 0x20, 0x95, 0x1c, 0xb9, 0x44, 0x4e,
-	0xe2, 0xba, 0xa3, 0x26, 0x71, 0x12, 0x3b, 0x8b, 0xfa, 0x0a, 0x7b, 0xe2, 0xb1, 0x38, 0xee, 0x91,
-	0x23, 0xda, 0x7d, 0x11, 0x64, 0xc7, 0x9b, 0x5d, 0xc4, 0x99, 0x4b, 0x92, 0x99, 0x6f, 0xc6, 0x99,
-	0xff, 0xf7, 0xe0, 0xa7, 0xaa, 0x55, 0xb4, 0x91, 0xb2, 0x54, 0x74, 0x19, 0x67, 0x5c, 0xb3, 0x78,
-	0x88, 0xa2, 0xa6, 0x93, 0x5a, 0x7a, 0x73, 0xd5, 0xaa, 0x68, 0x48, 0x38, 0x7c, 0x75, 0x21, 0xa4,
-	0x90, 0x96, 0x52, 0xf3, 0x35, 0x14, 0x5e, 0xcd, 0x59, 0x05, 0xb5, 0xa4, 0xf6, 0xe9, 0x52, 0x44,
-	0x48, 0x29, 0x4a, 0x4e, 0x6d, 0x94, 0xf5, 0xb7, 0xb4, 0xe8, 0x3b, 0xa6, 0x41, 0xd6, 0x3b, 0x9e,
-	0x4b, 0x55, 0x49, 0x45, 0x33, 0xa6, 0xf8, 0xf8, 0xf3, 0x5c, 0xc2, 0x8e, 0x5f, 0x3a, 0x5e, 0x29,
-	0x41, 0x97, 0xb1, 0x79, 0x39, 0xb0, 0x30, 0x33, 0xb7, 0x3d, 0xef, 0x1e, 0xf6, 0x33, 0x33, 0x01,
-	0xf5, 0xc1, 0xe1, 0x8b, 0xd5, 0x11, 0x3e, 0xff, 0xc8, 0xf5, 0x8d, 0x19, 0x3d, 0xe1, 0x6d, 0xcf,
-	0x95, 0xf6, 0x2e, 0xf1, 0x89, 0x91, 0x92, 0x42, 0xe1, 0xa3, 0xe0, 0x38, 0x9c, 0x26, 0x33, 0x13,
-	0x5e, 0x17, 0xde, 0x73, 0x3c, 0xaf, 0xa0, 0x4e, 0x4b, 0x68, 0x7b, 0x28, 0x40, 0x3f, 0xa4, 0x39,
-	0x6b, 0xfc, 0xa3, 0x00, 0x85, 0xd3, 0xe4, 0xbc, 0x82, 0xfa, 0xf3, 0x2e, 0xff, 0x8e, 0x35, 0xde,
-	0x6b, 0xfc, 0xf8, 0x3b, 0xe8, 0xbb, 0xb4, 0x62, 0xdd, 0x3d, 0xd7, 0x29, 0xd4, 0x39, 0xaf, 0x35,
-	0x2c, 0xb9, 0xf2, 0x8f, 0x03, 0x14, 0x9e, 0x26, 0x17, 0x86, 0x7e, 0xb1, 0xf0, 0x7a, 0x64, 0xde,
-	0x1b, 0x3c, 0xbb, 0x85, 0x52, 0xf3, 0xce, 0x9f, 0x06, 0x28, 0x3c, 0x7b, 0x49, 0xa2, 0x7f, 0x8c,
-	0x8d, 0xbe, 0x1a, 0x45, 0x1f, 0x6c, 0x55, 0xe2, 0xaa, 0xbd, 0xf7, 0x18, 0xef, 0xa5, 0xf9, 0x8f,
-	0x6c, 0xef, 0x33, 0xdb, 0x6b, 0xf5, 0x8f, 0xbd, 0x37, 0x63, 0x91, 0x13, 0x9b, 0x1c, 0xf4, 0x2d,
-	0x56, 0x08, 0x9f, 0x1d, 0x9c, 0xee, 0x3d, 0xc1, 0xa7, 0xce, 0x08, 0xe5, 0x9c, 0x38, 0x19, 0x9c,
-	0x50, 0xff, 0xdf, 0x8a, 0xb7, 0x9f, 0x7e, 0x6e, 0x08, 0x5a, 0x6f, 0x08, 0xfa, 0xbd, 0x21, 0xe8,
-	0xc7, 0x96, 0x4c, 0xd6, 0x5b, 0x32, 0xf9, 0xb5, 0x25, 0x93, 0x6f, 0xb1, 0x00, 0x7d, 0xd7, 0x67,
-	0x51, 0x2e, 0x2b, 0x6a, 0xaf, 0x1e, 0xd4, 0x8b, 0x92, 0x65, 0x8a, 0xda, 0x1d, 0xbd, 0x17, 0x94,
-	0x35, 0xf0, 0xf7, 0x96, 0x66, 0x33, 0x7b, 0xdb, 0xaf, 0xfe, 0x04, 0x00, 0x00, 0xff, 0xff, 0xca,
-	0xdb, 0x22, 0xd8, 0xc7, 0x02, 0x00, 0x00,
+	// 358 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x54, 0x91, 0xc1, 0x6a, 0xe3, 0x30,
+	0x10, 0x86, 0xa3, 0x4d, 0x36, 0xbb, 0x78, 0x0f, 0x21, 0x26, 0x6c, 0x4c, 0x60, 0x8d, 0x09, 0x7b,
+	0x30, 0x85, 0x5a, 0xb8, 0xed, 0x13, 0xb4, 0x85, 0x12, 0xda, 0x42, 0xf0, 0xb1, 0x17, 0x23, 0xdb,
+	0xaa, 0x32, 0xc4, 0xb6, 0x6c, 0x4b, 0x4e, 0xc9, 0x5b, 0xf4, 0xb1, 0x7a, 0x0c, 0x3d, 0xf5, 0x58,
+	0x92, 0x17, 0x29, 0x52, 0xec, 0x24, 0xbd, 0x48, 0x9a, 0xf9, 0x46, 0xd2, 0x3f, 0xff, 0x18, 0xff,
+	0x44, 0x29, 0x70, 0xc1, 0x79, 0x2a, 0xf0, 0xca, 0x8f, 0xa8, 0x24, 0xfe, 0x3e, 0xf2, 0x8a, 0x8a,
+	0x4b, 0x6e, 0x0e, 0x45, 0x29, 0xbc, 0x7d, 0xa2, 0xc1, 0x93, 0x11, 0xe3, 0x8c, 0x6b, 0x8a, 0xd5,
+	0x69, 0x5f, 0x38, 0x19, 0x92, 0x0c, 0x72, 0x8e, 0xf5, 0xda, 0xa4, 0x6c, 0xc6, 0x39, 0x4b, 0x29,
+	0xd6, 0x51, 0x54, 0x3f, 0xe3, 0xa4, 0xae, 0x88, 0x04, 0x9e, 0xb7, 0x3c, 0xe6, 0x22, 0xe3, 0x02,
+	0x47, 0x44, 0xd0, 0xc3, 0xe7, 0x31, 0x87, 0x96, 0x8f, 0x1b, 0x9e, 0x09, 0x86, 0x57, 0xbe, 0xda,
+	0x1a, 0x30, 0x55, 0x9a, 0xcb, 0x9a, 0x56, 0xeb, 0xa3, 0x66, 0xc2, 0x20, 0x3f, 0x79, 0x7c, 0xfa,
+	0x8e, 0x8c, 0xc1, 0x1d, 0x95, 0x73, 0x25, 0x3d, 0xa0, 0x65, 0x4d, 0x85, 0x34, 0xc7, 0xc6, 0x2f,
+	0xd5, 0x4a, 0x08, 0x89, 0x85, 0x9c, 0xae, 0xdb, 0x0b, 0xfa, 0x2a, 0x9c, 0x25, 0xe6, 0x99, 0x31,
+	0xcc, 0x20, 0x0f, 0x53, 0x28, 0x6b, 0x48, 0x40, 0xae, 0xc3, 0x98, 0x14, 0xd6, 0x0f, 0x07, 0xb9,
+	0xbd, 0x60, 0x90, 0x41, 0xfe, 0xd0, 0xe6, 0x6f, 0x48, 0x61, 0x5e, 0x19, 0x7f, 0x5f, 0x40, 0x2e,
+	0xc2, 0x8c, 0x54, 0x4b, 0x2a, 0x43, 0xc8, 0x63, 0x9a, 0x4b, 0x58, 0x51, 0x61, 0x75, 0x1d, 0xe4,
+	0xfe, 0x0e, 0x46, 0x8a, 0x3e, 0x6a, 0x38, 0x3b, 0x30, 0xf3, 0xd6, 0x30, 0x8e, 0x12, 0xad, 0x9f,
+	0x0e, 0x72, 0xff, 0x5c, 0xfc, 0xf7, 0x94, 0xb9, 0xba, 0x8f, 0xd6, 0x5c, 0x6f, 0x7e, 0x28, 0x6a,
+	0x44, 0x07, 0x27, 0xf7, 0xae, 0xef, 0xdf, 0xb6, 0x36, 0xda, 0x6c, 0x6d, 0xf4, 0xb9, 0xb5, 0xd1,
+	0xeb, 0xce, 0xee, 0x6c, 0x76, 0x76, 0xe7, 0x63, 0x67, 0x77, 0x9e, 0x7c, 0x06, 0x72, 0x51, 0x47,
+	0x5e, 0xcc, 0x33, 0xac, 0x5d, 0x03, 0x71, 0x9e, 0x92, 0x48, 0x60, 0x3d, 0xde, 0x25, 0xc3, 0xa4,
+	0x80, 0xef, 0x03, 0x8e, 0xfa, 0xda, 0xa8, 0xcb, 0xaf, 0x00, 0x00, 0x00, 0xff, 0xff, 0xd1, 0x35,
+	0x4c, 0xb9, 0x02, 0x02, 0x00, 0x00,
 }
 
 func (m *GetPoolsRequest) Marshal() (dAtA []byte, err error) {
@@ -244,18 +167,6 @@ func (m *GetPoolsRequest) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		i--
 		dAtA[i] = 0x2a
 	}
-	if m.Filter != nil {
-		{
-			size, err := m.Filter.MarshalToSizedBuffer(dAtA[:i])
-			if err != nil {
-				return 0, err
-			}
-			i -= size
-			i = encodeVarintPools(dAtA, i, uint64(size))
-		}
-		i--
-		dAtA[i] = 0x22
-	}
 	if m.WithMarketIncentives {
 		i--
 		if m.WithMarketIncentives {
@@ -272,76 +183,20 @@ func (m *GetPoolsRequest) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		dAtA[i] = 0x10
 	}
 	if len(m.PoolId) > 0 {
-		dAtA4 := make([]byte, len(m.PoolId)*10)
-		var j3 int
+		dAtA3 := make([]byte, len(m.PoolId)*10)
+		var j2 int
 		for _, num := range m.PoolId {
 			for num >= 1<<7 {
-				dAtA4[j3] = uint8(uint64(num)&0x7f | 0x80)
+				dAtA3[j2] = uint8(uint64(num)&0x7f | 0x80)
 				num >>= 7
-				j3++
+				j2++
 			}
-			dAtA4[j3] = uint8(num)
-			j3++
+			dAtA3[j2] = uint8(num)
+			j2++
 		}
-		i -= j3
-		copy(dAtA[i:], dAtA4[:j3])
-		i = encodeVarintPools(dAtA, i, uint64(j3))
-		i--
-		dAtA[i] = 0xa
-	}
-	return len(dAtA) - i, nil
-}
-
-func (m *QueryFilter) Marshal() (dAtA []byte, err error) {
-	size := m.Size()
-	dAtA = make([]byte, size)
-	n, err := m.MarshalToSizedBuffer(dAtA[:size])
-	if err != nil {
-		return nil, err
-	}
-	return dAtA[:n], nil
-}
-
-func (m *QueryFilter) MarshalTo(dAtA []byte) (int, error) {
-	size := m.Size()
-	return m.MarshalToSizedBuffer(dAtA[:size])
-}
-
-func (m *QueryFilter) MarshalToSizedBuffer(dAtA []byte) (int, error) {
-	i := len(dAtA)
-	_ = i
-	var l int
-	_ = l
-	if m.WithMarketIncentives {
-		i--
-		if m.WithMarketIncentives {
-			dAtA[i] = 1
-		} else {
-			dAtA[i] = 0
-		}
-		i--
-		dAtA[i] = 0x18
-	}
-	if m.MinLiquidityCap != 0 {
-		i = encodeVarintPools(dAtA, i, uint64(m.MinLiquidityCap))
-		i--
-		dAtA[i] = 0x10
-	}
-	if len(m.PoolIds) > 0 {
-		dAtA6 := make([]byte, len(m.PoolIds)*10)
-		var j5 int
-		for _, num := range m.PoolIds {
-			for num >= 1<<7 {
-				dAtA6[j5] = uint8(uint64(num)&0x7f | 0x80)
-				num >>= 7
-				j5++
-			}
-			dAtA6[j5] = uint8(num)
-			j5++
-		}
-		i -= j5
-		copy(dAtA[i:], dAtA6[:j5])
-		i = encodeVarintPools(dAtA, i, uint64(j5))
+		i -= j2
+		copy(dAtA[i:], dAtA3[:j2])
+		i = encodeVarintPools(dAtA, i, uint64(j2))
 		i--
 		dAtA[i] = 0xa
 	}
@@ -378,35 +233,9 @@ func (m *GetPoolsRequest) Size() (n int) {
 	if m.WithMarketIncentives {
 		n += 2
 	}
-	if m.Filter != nil {
-		l = m.Filter.Size()
-		n += 1 + l + sovPools(uint64(l))
-	}
 	if m.Pagination != nil {
 		l = m.Pagination.Size()
 		n += 1 + l + sovPools(uint64(l))
-	}
-	return n
-}
-
-func (m *QueryFilter) Size() (n int) {
-	if m == nil {
-		return 0
-	}
-	var l int
-	_ = l
-	if len(m.PoolIds) > 0 {
-		l = 0
-		for _, e := range m.PoolIds {
-			l += sovPools(uint64(e))
-		}
-		n += 1 + sovPools(uint64(l)) + l
-	}
-	if m.MinLiquidityCap != 0 {
-		n += 1 + sovPools(uint64(m.MinLiquidityCap))
-	}
-	if m.WithMarketIncentives {
-		n += 2
 	}
 	return n
 }
@@ -561,42 +390,6 @@ func (m *GetPoolsRequest) Unmarshal(dAtA []byte) error {
 				}
 			}
 			m.WithMarketIncentives = bool(v != 0)
-		case 4:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Filter", wireType)
-			}
-			var msglen int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowPools
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				msglen |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			if msglen < 0 {
-				return ErrInvalidLengthPools
-			}
-			postIndex := iNdEx + msglen
-			if postIndex < 0 {
-				return ErrInvalidLengthPools
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			if m.Filter == nil {
-				m.Filter = &QueryFilter{}
-			}
-			if err := m.Filter.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
-				return err
-			}
-			iNdEx = postIndex
 		case 5:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Pagination", wireType)
@@ -633,171 +426,6 @@ func (m *GetPoolsRequest) Unmarshal(dAtA []byte) error {
 				return err
 			}
 			iNdEx = postIndex
-		default:
-			iNdEx = preIndex
-			skippy, err := skipPools(dAtA[iNdEx:])
-			if err != nil {
-				return err
-			}
-			if (skippy < 0) || (iNdEx+skippy) < 0 {
-				return ErrInvalidLengthPools
-			}
-			if (iNdEx + skippy) > l {
-				return io.ErrUnexpectedEOF
-			}
-			iNdEx += skippy
-		}
-	}
-
-	if iNdEx > l {
-		return io.ErrUnexpectedEOF
-	}
-	return nil
-}
-func (m *QueryFilter) Unmarshal(dAtA []byte) error {
-	l := len(dAtA)
-	iNdEx := 0
-	for iNdEx < l {
-		preIndex := iNdEx
-		var wire uint64
-		for shift := uint(0); ; shift += 7 {
-			if shift >= 64 {
-				return ErrIntOverflowPools
-			}
-			if iNdEx >= l {
-				return io.ErrUnexpectedEOF
-			}
-			b := dAtA[iNdEx]
-			iNdEx++
-			wire |= uint64(b&0x7F) << shift
-			if b < 0x80 {
-				break
-			}
-		}
-		fieldNum := int32(wire >> 3)
-		wireType := int(wire & 0x7)
-		if wireType == 4 {
-			return fmt.Errorf("proto: QueryFilter: wiretype end group for non-group")
-		}
-		if fieldNum <= 0 {
-			return fmt.Errorf("proto: QueryFilter: illegal tag %d (wire type %d)", fieldNum, wire)
-		}
-		switch fieldNum {
-		case 1:
-			if wireType == 0 {
-				var v uint64
-				for shift := uint(0); ; shift += 7 {
-					if shift >= 64 {
-						return ErrIntOverflowPools
-					}
-					if iNdEx >= l {
-						return io.ErrUnexpectedEOF
-					}
-					b := dAtA[iNdEx]
-					iNdEx++
-					v |= uint64(b&0x7F) << shift
-					if b < 0x80 {
-						break
-					}
-				}
-				m.PoolIds = append(m.PoolIds, v)
-			} else if wireType == 2 {
-				var packedLen int
-				for shift := uint(0); ; shift += 7 {
-					if shift >= 64 {
-						return ErrIntOverflowPools
-					}
-					if iNdEx >= l {
-						return io.ErrUnexpectedEOF
-					}
-					b := dAtA[iNdEx]
-					iNdEx++
-					packedLen |= int(b&0x7F) << shift
-					if b < 0x80 {
-						break
-					}
-				}
-				if packedLen < 0 {
-					return ErrInvalidLengthPools
-				}
-				postIndex := iNdEx + packedLen
-				if postIndex < 0 {
-					return ErrInvalidLengthPools
-				}
-				if postIndex > l {
-					return io.ErrUnexpectedEOF
-				}
-				var elementCount int
-				var count int
-				for _, integer := range dAtA[iNdEx:postIndex] {
-					if integer < 128 {
-						count++
-					}
-				}
-				elementCount = count
-				if elementCount != 0 && len(m.PoolIds) == 0 {
-					m.PoolIds = make([]uint64, 0, elementCount)
-				}
-				for iNdEx < postIndex {
-					var v uint64
-					for shift := uint(0); ; shift += 7 {
-						if shift >= 64 {
-							return ErrIntOverflowPools
-						}
-						if iNdEx >= l {
-							return io.ErrUnexpectedEOF
-						}
-						b := dAtA[iNdEx]
-						iNdEx++
-						v |= uint64(b&0x7F) << shift
-						if b < 0x80 {
-							break
-						}
-					}
-					m.PoolIds = append(m.PoolIds, v)
-				}
-			} else {
-				return fmt.Errorf("proto: wrong wireType = %d for field PoolIds", wireType)
-			}
-		case 2:
-			if wireType != 0 {
-				return fmt.Errorf("proto: wrong wireType = %d for field MinLiquidityCap", wireType)
-			}
-			m.MinLiquidityCap = 0
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowPools
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				m.MinLiquidityCap |= uint64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-		case 3:
-			if wireType != 0 {
-				return fmt.Errorf("proto: wrong wireType = %d for field WithMarketIncentives", wireType)
-			}
-			var v int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowPools
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				v |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			m.WithMarketIncentives = bool(v != 0)
 		default:
 			iNdEx = preIndex
 			skippy, err := skipPools(dAtA[iNdEx:])
