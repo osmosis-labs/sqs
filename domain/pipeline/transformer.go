988e9f78af51f97b7cdd76a646474f8f75816a30
Deividas Petraitis <hi@deividaspetraitis.lt>
2024-11-28 16:46:10 +0200

    BE-636-pipeline | Fix method signatures and return types in Transformer interface

--- domain/pipeline/transformer.go
+++ domain/pipeline/transformer.go
@@ -7,9 +7,9 @@ import (
 
 // Transformer defines a generic interface for filtering and sorting data.
 type Transformer[K, V any] interface {
-	Filter(fn func(V) bool) *Transformer[K, V]       // Filter applies a filter to the data
-	Sort(less ...func(V, V) bool) *Transformer[K, V] // Sort sorts the data
-	Keys() []string                                  // Keys returns the list of transformed keys
+	Filter(fn func(V) bool) Transformer[K, V]       // Filter applies a filter to the data
+	Sort(less ...func(V, V) bool) Transformer[K, V] // Sort sorts the data
+	Keys() []string                                 // Keys returns the list of transformed keys
 }
 
 // SyncMapTransformer is a generic data transformer for map data
