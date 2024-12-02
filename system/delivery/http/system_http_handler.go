4986e8de3709865d8a241278de4688d300fa9d06
Deividas Petraitis <hi@deividaspetraitis.lt>
2024-12-02 10:33:16 +0200

    BE-636 | Omit swagger related changes

--- system/delivery/http/system_http_handler.go
+++ system/delivery/http/system_http_handler.go
@@ -15,7 +15,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	echoSwagger "github.com/swaggo/echo-swagger"
 
-	"github.com/osmosis-labs/sqs/client/docs"
 	"github.com/osmosis-labs/sqs/domain"
 	"github.com/osmosis-labs/sqs/domain/mvc"
 	"github.com/osmosis-labs/sqs/log"
@@ -80,7 +79,6 @@ func NewSystemHandler(e *echo.Echo, config domain.Config, logger log.Logger, us 
 	e.GET("/version", handler.GetVersion)
 	e.GET("/metrics", echo.WrapHandler(promhttp.Handler()))
 	e.GET("/swagger/*", echoSwagger.EchoWrapHandler(echoSwagger.URL("docs/swagger.json"), echoSwagger.URL("swagger.yaml")))
-	e.GET("/swagger1/*", echo.WrapHandler(http.StripPrefix("/swagger1/", http.FileServer(http.FS(docs.SwaggerUI)))))
 }
 
 // GetConfig returns the config for the SQS service
