fa7aef9a8f9337c3c35d10cb83957cb322c95aa8
Deividas Petraitis <hi@deividaspetraitis.lt>
2025-01-22 18:03:45 +0200

    BE-676 | Update ConcentratedNotEnoughLiquidityToCompleteSwapError for AmountOut error

--- domain/errors.go
+++ domain/errors.go
@@ -143,7 +143,11 @@ type ConcentratedNotEnoughLiquidityToCompleteSwapError struct {
 }
 
 func (e ConcentratedNotEnoughLiquidityToCompleteSwapError) Error() string {
-	return fmt.Sprintf("not enough liquidity to complete swap in pool (%d) with amount in (%s)", e.PoolId, e.AmountIn)
+	if e.AmountIn != "" {
+		return fmt.Sprintf("not enough liquidity to complete swap in pool (%d) with amount in (%s)", e.PoolId, e.AmountIn)
+	}
+
+	return fmt.Sprintf("not enough liquidity to complete swap in pool (%d) with amount out (%s)", e.PoolId, e.AmountOut)
 }
 
 type ConcentratedTickModelNotSetError struct {
